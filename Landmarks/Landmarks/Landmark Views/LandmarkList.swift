727913056e2f90b93b367130e1ae5f1132475673
m-kodama <m-kodama@rhizo-me.com>
2019-09-30 15:46:15 +0900

    complete section 4

---
+++ Landmarks/Landmarks/Landmark Views/LandmarkList.swift
@@ -0,0 +1,24 @@
+//
+//  LandmarkList.swift
+//  Landmarks
+//
+//  Created by m-kodama on 2019/09/30.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct LandmarkList: View {
+    var body: some View {
+        List {
+            LandmarkRow(landmark: landmarkData[0])
+            LandmarkRow(landmark: landmarkData[1])
+        }
+    }
+}
+
+struct LandmarkList_Previews: PreviewProvider {
+    static var previews: some View {
+        LandmarkList()
+    }
+}
